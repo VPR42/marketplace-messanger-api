88e7880a8d38d79791c2f47c59a07fb39a4d121c
Запара Иван <truthpruthbg@gmail.com>
2025-11-30 12:57:44 +0300

    VJ-112: Подправлен пайплайн для понижения простоя сервиса при деплое + исправлены типы запроса

--- src/main/kotlin/com/vpr42/marketplacemessangerapi/controller/ChatController.kt
+++ src/main/kotlin/com/vpr42/marketplacemessangerapi/controller/ChatController.kt
@@ -7,10 +7,12 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
-import java.util.UUID
+import java.util.*
 
 @Controller
 @RequestMapping("/api/chat")
@@ -23,7 +25,7 @@ class ChatController(
 ) {
     private val logger = LoggerFactory.getLogger(ChatController::class.java)
 
-    @RequestMapping("/create/{id}")
+    @PostMapping("/create/{id}")
     @Operation(description = "Метод создания чата к заказу")
     fun createChat(
         @RequestHeader("id") userId: String,
@@ -35,7 +37,7 @@ class ChatController(
         )
     }
 
-    @RequestMapping("/close/{id}")
+    @PatchMapping("/close/{id}")
     @Operation(description = "Метод закрытия чата к заказу")
     fun closeChat(
         @PathVariable("id") orderId: String
